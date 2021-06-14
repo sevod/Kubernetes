409572ad8750ab170ec0b3b9bc5dc071e22dabc5
Sevod <ssevod@gmail.com>
2021-06-14 20:21:25 +0300

    mysql add mysql docker container

---
+++ dockers/mysql/srcs/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mysqld
+
+#wordpress-SQL
+#echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql -u root --skip-password
+echo "source /localhost.sql;" | mysql -u root --skip-password
+echo "GRANT ALL ON wordpress.* TO 'root'@'localhost' IDENTIFIED BY '123456';" | mysql -u root --skip-password
